2f305d2f0b6e5772ee25b4b1ad36e09e2abbf351
Ali Al-Zuhairi <eli.zuhairi@gmail.com>
2025-04-19 19:26:46 +0300

    add themes and new blog design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,33 @@ module.exports = {
         poppins: ['var(--font-poppins)'],
         roboto: ['var(--font-roboto)'],
       },
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary)',
+          hover: 'var(--primary-hover)',
+        },
+      },
+      backgroundColor: {
+        'theme': 'var(--background)',
+        'primary': 'var(--primary)',
+        'primary-hover': 'var(--primary-hover)',
+        'primary-glow': 'var(--primary-glow)',
+      },
+      textColor: {
+        'theme': 'var(--foreground)',
+        'primary': 'var(--primary)',
+      },
+      gradientColorStops: {
+        'start': 'var(--gradient-start)',
+        'mid': 'var(--gradient-mid)',
+        'end': 'var(--gradient-end)',
+      },
+      borderColor: {
+        'primary': 'var(--primary)',
+      },
+      ringColor: {
+        'primary': 'var(--primary)',
+      },
     },
   },
   plugins: [
