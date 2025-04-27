7c6645af3d7cd39d50ac45fbaec84eac87176329
Ali Al-Zuhairi <eli.zuhairi@gmail.com>
2025-04-27 08:05:37 +0300

    fixing icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ const Icon = ({ name, className = "" }: { name: string, className?: string }) =>
       </svg>
     ),
     link: (
-      <svg xmlns="http://www.w3.org/2000/svg" className={`w-5 h-5 ${className}`} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <svg xmlns="http://www.w3.org/2000/svg" className={`w-full h-full text-primary/40 ${className}`} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
         <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
       </svg>
